5c4b6ac7a3de04cc27416ab642199073e0d98378
Max Lynch <max@ionic.io>
2020-06-08 16:58:42 -0500

    Trying older stencil version

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,7 +5,7 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-import { MarkdownContent, MarkdownHeading, SiteStructureItem } from "./global/definitions";
+import { MarkdownContent, MarkdownHeading, SiteStructureItem, } from "./global/definitions";
 export namespace Components {
     interface AnchorLink {
         "to": string;
