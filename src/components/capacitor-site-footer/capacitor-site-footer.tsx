5179f75b8f8b29b3d50ab6d39107a25864aa7418
Max Lynch <max@ionic.io>
2020-06-08 23:33:52 -0500

    Footer

--- src/components/capacitor-site-footer/capacitor-site-footer.tsx
+++ src/components/capacitor-site-footer/capacitor-site-footer.tsx
@@ -1,8 +1,9 @@
 import { Component, Host, h } from '@stencil/core';
+import { ResponsiveContainer, Grid, Col, Heading } from '@ionic-internal/sites-shared';
 
 @Component({
   tag: 'capacitor-site-footer',
-  styleUrl: 'capacitor-site-footer.css',
+  styleUrl: 'capacitor-site-footer.scss',
   scoped: true,
 })
 export class CapacitorSiteFooter {
@@ -10,41 +11,45 @@ export class CapacitorSiteFooter {
     return (
       <Host>
         <footer>
-          <div class="container">
-            <div id="open-source">
-              <a href="http://ionicframework.com/" title="IonicFramework.com" rel="noopener">
-                <div class="ionic-oss-logo"></div>
-              </a>
-              <p>Released under <span id="mit">MIT License</span> | Copyright @ {(new Date()).getFullYear()} Drifty Co.</p>
-            </div>
-
-            <div id="footer-icons">
-              <iframe
-                title="Github Star Count" 
-                class="star-button"
-                src="https://ghbtns.com/github-btn.html?user=ionic-team&repo=capacitor&type=star&count=true"
-                frameBorder="0"
-                scrolling="0"
-                width="100px"
-                height="20px"
-              ></iframe>
-
-              <a class="svg-button"
-                id="capacitor-twitter"
-                href="https://twitter.com/getcapacitor"
-                target="_blank"
-                rel="noopener"
-                title="Open the Capacitor account on twitter"
-                style={{fill: 'white'}}
-                >
-                <app-icon name="twitter"></app-icon>
-              </a>
-              <a class="svg-button" id="cap-forum" href="https://getcapacitor.herokuapp.com/" target="_blank" rel="noopener"
-                title="Join the Capacitor slack">
-                <app-icon name="slack"></app-icon>
-              </a>
-            </div>
-          </div>
+          <ResponsiveContainer>
+            <Grid>
+              <Col md={6} sm={6} xs={12} cols={12} class="copyright">
+                <img src="/assets/img/logo-white2.png" alt="Capacitor Logo" class="logo" />
+                <p>
+                  Copyright @ {(new Date()).getFullYear()} Drifty Co.
+                </p>
+                <p><a href="https://ionic.io">Ionic Open Source</a> | Released under <span id="mit">MIT License</span></p>
+              </Col>
+              <Col md={6} sm={6} xs={12} cols={12}>
+                <Grid>
+                  <Col md={4} sm={4} xs={4} cols={4}>
+                    <Heading level={5}>Developers</Heading>
+                    <ul>
+                      <li><a href="/docs/getting-started">Install</a></li>
+                      <li><a href="/docs">Docs</a></li>
+                      <li><a href="/docs/apis">APIs</a></li>
+                    </ul>
+                  </Col>
+                  <Col md={4} sm={4} xs={4} cols={4}>
+                    <Heading level={5}>Resources</Heading>
+                    <ul>
+                      <li><a href="/community">Community</a></li>
+                      <li><a href="/blog">Blog</a></li>
+                      <li><a href="https://github.com/ionic-team/capacitor/discussions">Discussions</a></li>
+                    </ul>
+                  </Col>
+                  <Col md={4} sm={4} xs={4} cols={4}>
+                    <Heading level={5}>Connect</Heading>
+                    <ul>
+                      <li><a href="https://github.com/ionic-team/capacitor">GitHub</a></li>
+                      <li><a href="https://twitter.com/capacitorjs">Twitter</a></li>
+                      <li><a href="https://ionic.io">Ionic</a></li>
+                    </ul>
+                  </Col>
+                </Grid>
+              </Col>
+            </Grid>
+          </ResponsiveContainer>
         </footer>
       </Host>
     );
