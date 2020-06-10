e38c8934654af2caf2f6de7a4493a19e333a02da
Max Lynch <max@ionic.io>
2020-06-10 09:55:34 -0500

    Fix the build

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.slugify = void 0;
 const chalk_1 = __importDefault(require("chalk"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
