b813fe33c61aeb06df6b696f2b24406430ddce06
Max Lynch <max@ionic.io>
2020-06-10 22:45:55 -0500

    Native features

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -3,6 +3,7 @@ import { Component, h, Host, State } from '@stencil/core';
 import Helmet from '@stencil/helmet';
 import { ResponsiveContainer, Grid, Col, AnchorButton, Heading, Paragraph } from '@ionic-internal/sites-shared';
 import { Tabs, Tab, TabBar, TabBarButton } from '../tabs';
+import FancyUnderline from '../FancyUnderline';
 
 @Component({
   tag: 'landing-page',
@@ -120,6 +121,105 @@ export class LandingPage {
               </Col>
             </Grid>
           </section>
+          <section class="section--web-apps-to-native">
+            <hgroup>
+              <Heading level={3}>
+                Connect web apps to<br />
+                <FancyUnderline>native functionality.</FancyUnderline>
+              </Heading>
+            </hgroup>
+            <Grid>
+              <Col md={4} sm={4} xs={12} cols={12}>
+                <img src="/assets/img/landing/universal-apps.png" alt="Universal apps" />
+                <Heading level={4}>
+                  Universal apps
+                </Heading>
+                <Paragraph>
+                  Build web-based applications that run equally well across iOS, Android, and as Progressive Web Apps.
+                </Paragraph>
+              </Col>
+              <Col md={4} sm={4} xs={12} cols={12}>
+                <img src="/assets/img/landing/native-access.png" alt="Native access" />
+                <Heading level={4}>
+                  Native access
+                </Heading>
+                <Paragraph>
+                  Access the full Native SDKs on each platform, and easily deploy to the App Stores (and the web).
+                </Paragraph>
+              </Col>
+              <Col md={4} sm={4} xs={12} cols={12}>
+                <img src="/assets/img/landing/native-pwas.png" alt="Native PWAs" />
+                <Heading level={4}>
+                  Native PWAs
+                </Heading>
+                <Paragraph>
+                  Add custom native functionality with a simple Plugin API, or use existing Cordova plugins with our compatibility layer.
+                </Paragraph>
+              </Col>
+            </Grid>
+          </section>
+          <section class="section--native-features">
+            <hgroup>
+              <Heading level={3}>
+                Cross-platform core<br />
+                <FancyUnderline>native features</FancyUnderline>
+              </Heading>
+            </hgroup>
+            <Grid>
+              {[
+                { 
+                  key: 'camera',
+                  name: 'Camera',
+                  desc: 'Capture, save photos, and configure hardware parameters like focus and white balance.'
+                },
+                {
+                  key: 'filesystem',
+                  name: 'File System',
+                  desc: 'Save and read assets, documents, and other data your users need by accessing native file systems'
+                },
+                {
+                  key: 'geolocation',
+                  name: 'Geolocation',
+                  desc: 'Build location-aware apps by polling for the current device location or subscribing to location updates.'
+                },
+                {
+                  key: 'accelerometer',
+                  name: 'Accelerometer',
+                  desc: 'Access the device accelerometer sensors to respond to changes in device motion in 3d space.'
+                },
+                {
+                  key: 'notifications',
+                  name: 'Notifications',
+                  desc: 'Build applications that send and respond to local and server-pushed notifications'
+                },
+                {
+                  key: 'haptics',
+                  name: 'Haptics',
+                  desc: 'Use haptic hardware to provide physical feedback for user actions'
+                },
+                {
+                  key: 'accessibility',
+                  name: 'Accessibility',
+                  desc: 'Respond to changes in accessibility states and extend your app with a11y features'
+                },
+                {
+                  key: 'custom',
+                  name: 'Your Own Plugin',
+                  desc: 'Extend your app with custom native and web code to provide consistent APIs across platforms.'
+                }
+              ].map(f => (
+              <Col md={3} sm={3} xs={6} cols={12} key={f.key}>
+                <img src={`/assets/img/landing/native-${f.key}.png`} alt={f.name} />
+                <Heading level={4}>
+                  {f.name}
+                </Heading>
+                <Paragraph>
+                  {f.desc}
+                </Paragraph>
+              </Col>
+              ))}
+            </Grid>
+          </section>
           <section class="section--platforms">
             <hgroup>
               <Heading level={3}>
@@ -127,7 +227,7 @@ export class LandingPage {
                 All from a single codebase.
               </Heading>
               <Paragraph>
-                Build cross-platform apps that work seemlessly across iOS, Android, desktop, and the web. Reduce maintenance and development time with a powerful app foundation that lets you build once and deploy anywhere.
+                Build cross-platform apps that work seamlessly across iOS, Android, desktop, and the web. Reduce maintenance and development time with a powerful app foundation that lets you build once and deploy anywhere.
               </Paragraph>
             </hgroup>
             <div class="section--platforms__all">
