71e3ca332a5b3c7fd2d2ab582785c006f12cad71
Brandy Carney <brandy@ionic.io>
2020-06-18 11:04:04 -0400

    fix(header): sticky header

--- src/components/docs-header/docs-header.tsx
+++ src/components/docs-header/docs-header.tsx
@@ -1,4 +1,4 @@
-import { Component, ComponentInterface, h } from '@stencil/core';
+import { Component, ComponentInterface, Host, h } from '@stencil/core';
 import { href } from 'stencil-router-v2';
 import Router from '../../router';
 
@@ -14,23 +14,25 @@ export class DocsHeader implements ComponentInterface {
   }
 
   render() {
-    return [
-      <docs-search></docs-search>,
-      <div class="docs-header-links">
-        <a {...href('/docs')} class={{ 'active': this.isActive('/docs') }}>Docs</a>
-        <a {...href('/plugins')} class={{ 'active': this.isActive('/plugins') }}>API Plugins</a>
-        <a {...href('/blog')}>Blog</a>
-        <a {...href('/community')}>Community</a>
-      </div>,
-      <div class="docs-header-link-divider"/>,
-      <div class="docs-header-external-links">
-        <a rel="noopener" target="_blank" href="https://twitter.com/capacitorjs" aria-label="Twitter">
-          <ion-icon name="logo-twitter"></ion-icon>
-        </a>
-        <a rel="noopener" target="_blank" href="https://github.com/ionic-team/capacitor" aria-label="GitHub">
-          <ion-icon name="logo-github"></ion-icon>
-        </a>
-      </div>
-    ];
+    return (
+      <Host>
+        <docs-search></docs-search>
+        <div class="docs-header-links">
+          <a {...href('/docs')} class={{ 'active': this.isActive('/docs') }}>Docs</a>
+          <a {...href('/plugins')} class={{ 'active': this.isActive('/plugins') }}>API Plugins</a>
+          <a {...href('/blog')}>Blog</a>
+          <a {...href('/community')}>Community</a>
+        </div>
+        <div class="docs-header-link-divider"/>
+        <div class="docs-header-external-links">
+          <a rel="noopener" target="_blank" href="https://twitter.com/capacitorjs" aria-label="Twitter">
+            <ion-icon name="logo-twitter"></ion-icon>
+          </a>
+          <a rel="noopener" target="_blank" href="https://github.com/ionic-team/capacitor" aria-label="GitHub">
+            <ion-icon name="logo-github"></ion-icon>
+          </a>
+        </div>
+      </Host>
+    );
   }
 }
