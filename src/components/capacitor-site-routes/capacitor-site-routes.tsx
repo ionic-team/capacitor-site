b519bbe591324d000d8e9772a253e0c6a9a66dd4
jaredcbaum <jaredcbaum@gmail.com>
2020-10-10 18:40:10 -0500

    fix(): Remove unused import

--- src/components/capacitor-site-routes/capacitor-site-routes.tsx
+++ src/components/capacitor-site-routes/capacitor-site-routes.tsx
@@ -6,7 +6,6 @@ import { InternalRouterState } from 'stencil-router-v2/dist/types';
 
 import state from '../../store';
 import Router from '../../router';
-import Helmet from '@stencil/helmet';
 
 @Component({
   tag: 'capacitor-site-routes',
