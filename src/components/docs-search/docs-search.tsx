10e75af78d3673da602b6a4d3bc74e57a1327b5d
Jared B <jaredcbaum@gmail.com>
2020-10-18 20:51:43 -0500

    feat(docs): Desktop search styling looking good

--- src/components/docs-search/docs-search.tsx
+++ src/components/docs-search/docs-search.tsx
@@ -80,7 +80,7 @@ export class DocsSearch implements ComponentInterface {
       apiKey: 'b3d47db9759a0a5884cf7807e23c77c5',
       indexName: `capacitorjs`,
       inputSelector: `#input-${this.uniqueId}`,
-      debug: false, // Set debug to true if you want to inspect the dropdown
+      debug: true, // Set debug to true if you want to inspect the dropdown
       queryHook: () => {
         if (this.input.isPristine) {
           this.input.isPristine = false;
