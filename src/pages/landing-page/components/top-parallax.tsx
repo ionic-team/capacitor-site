1283bad2839898c350bae6aa3c585523cdef6a50
Ely Lucas <ely@meta-tek.net>
2021-05-14 14:22:24 -0600

    fmt

--- src/pages/landing-page/components/top-parallax.tsx
+++ src/pages/landing-page/components/top-parallax.tsx
@@ -1,10 +1,7 @@
 import { Component, h } from '@stencil/core';
-import {
-  ResponsiveContainer,
-  Button,
-} from '@ionic-internal/ionic-ds';
+import { ResponsiveContainer, Button } from '@ionic-internal/ionic-ds';
 
-import Parallax from 'parallax-js'
+import Parallax from 'parallax-js';
 
 import Capacitor3Rainbow from '../assets/capacitor-3-rainbow.svg';
 import { Background } from '../assets/bg-3-only.svg';
@@ -37,19 +34,19 @@ export class LandingPage {
     return (
       <section id="top">
         <ResponsiveContainer>
-          <div id="shapes" ref={(el) => this.$shapes = el as HTMLDivElement}>
-            <div id="circle-1" data-depth="0.8"/>
-            <div id="circle-2" data-depth="0.3"/>
-            <div id="circle-3" data-depth="0.6"/>
-            <div id="square-1" data-depth="0.7"/>
-            <div id="square-2" data-depth="0.7"/>
-            <div id="square-3" data-depth="0.6"/>
-            <div id="square-4" data-depth="0.3"/>
-            <div id="square-5" data-depth="0.1"/>
-            <div id="square-6" data-depth="0.2"/>
-            <div id="square-7" data-depth="0.3"/>
-            <div id="square-8" data-depth="0.5"/>
-            <Background id="bg" data-depth="0.3"/>
+          <div id="shapes" ref={el => (this.$shapes = el as HTMLDivElement)}>
+            <div id="circle-1" data-depth="0.8" />
+            <div id="circle-2" data-depth="0.3" />
+            <div id="circle-3" data-depth="0.6" />
+            <div id="square-1" data-depth="0.7" />
+            <div id="square-2" data-depth="0.7" />
+            <div id="square-3" data-depth="0.6" />
+            <div id="square-4" data-depth="0.3" />
+            <div id="square-5" data-depth="0.1" />
+            <div id="square-6" data-depth="0.2" />
+            <div id="square-7" data-depth="0.3" />
+            <div id="square-8" data-depth="0.5" />
+            <Background id="bg" data-depth="0.3" />
           </div>
           <Capacitor3Rainbow />
           <h1>
