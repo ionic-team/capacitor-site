09a35c612dd3b835e2839c4455d410a6f9d03682
John Wheeler <73669769+johnwheeler-prod@users.noreply.github.com>
2022-09-21 14:08:30 -0400

    chore(portals for capacitor top bar): remove blue bg and try to asses… (#456)
    * chore(portals for capacitor top bar): remove blue bg and try to assess background image weirdness
    
    * chore(portals for capacitor top bar): switch to absolutely positioned svg for bg
    
    * chore(portals for cap top bar): fix button text color
    
    * chore(portals for cap top bar): preserve aspect ratio none as temp fix
    
    * chore(portals for cap bar): change preserve aspect ratio parameter
    
    * feat(): Finish cap banner
    
    Co-authored-by: jared <jaredcbaum@gmail.com>

--- src/components/portals-for-capacitor-bar/portals-for-capacitor-bar.tsx
+++ src/components/portals-for-capacitor-bar/portals-for-capacitor-bar.tsx
@@ -9,6 +9,289 @@ export class PortalsForCapacitorBar implements ComponentInterface {
   render() {
     return (
       <Host>
+        <div class="bg-container">
+          <svg
+            viewBox="0 0 1800 48"
+            height="48"
+            width="100%"
+            preserveAspectRatio="xMidYMid slice"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path fill="url(#a)" d="M0 0h1800v48H0z" />
+            <g opacity=".4">
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 787.4 -93.62)"
+                fill="url(#b)"
+              />
+              <rect
+                opacity=".5"
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 1672.5 -93.62)"
+                fill="url(#c)"
+              />
+              <rect
+                opacity=".7"
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 640.48 -259.82)"
+                fill="url(#d)"
+              />
+              <rect
+                opacity=".3"
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 1525.59 -259.82)"
+                fill="url(#e)"
+              />
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 640.48 63.79)"
+                fill="url(#f)"
+              />
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 1525.59 63.79)"
+                fill="url(#g)"
+              />
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 493.56 21.81)"
+                fill="url(#h)"
+              />
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 1378.67 21.81)"
+                fill="url(#i)"
+              />
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 345.83 -85.55)"
+                fill="url(#j)"
+              />
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 1230.94 -49.67)"
+                fill="url(#k)"
+              />
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 198.92 -92.82)"
+                fill="url(#l)"
+              />
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 1084.03 -118.18)"
+                fill="url(#m)"
+              />
+              <rect
+                opacity=".8"
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 52 -175.06)"
+                fill="url(#n)"
+              />
+              <rect
+                width="143.49"
+                height="290.61"
+                rx="28"
+                transform="matrix(.86603 -.5 0 1 937.11 -29.3)"
+                fill="url(#o)"
+              />
+            </g>
+            <defs>
+              <linearGradient
+                id="a"
+                x1="0"
+                y1="24"
+                x2="1800"
+                y2="24"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0038C9" />
+                <stop offset="1" stop-color="#53BAFF" />
+              </linearGradient>
+              <linearGradient
+                id="b"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="c"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="d"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="e"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="f"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="g"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="h"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="i"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="j"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="k"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="l"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#002992" />
+              </linearGradient>
+              <linearGradient
+                id="m"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+              <linearGradient
+                id="n"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#002279" />
+              </linearGradient>
+              <linearGradient
+                id="o"
+                x1="71.75"
+                y1="0"
+                x2="71.75"
+                y2="290.61"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stop-color="#0037C5" stop-opacity=".29" />
+                <stop offset="1" stop-color="#0037C5" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
         <nav>
           <h6>NEW</h6>
           <p>Introducing Portals for Capacitor</p>
